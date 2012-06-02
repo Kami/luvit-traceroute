1c0e0760941a9d4ef2578f6e2871ab6a25fb6e3a
Tomaz Muraus <tomaz@tomaz.me>
2012-06-02 13:36:08 -0700

    Add license, package and readme file.

---
+++ README.md
@@ -0,0 +1,7 @@
+# lua-traceroute
+
+Luvit wrapper for `traceroute` command.
+
+# License
+
+Library is distributed under the [Apache license](http://www.apache.org/licenses/LICENSE-2.0.html).
---
+++ package.lua
@@ -0,0 +1,8 @@
+return {
+  name = "traceroute",
+  version = "0.1.0",
+  description = "test library",
+  author = "Tomaz Muraus <tomaz@tomaz.me>",
+  contributors = {},
+  dependencies = {}
+}
