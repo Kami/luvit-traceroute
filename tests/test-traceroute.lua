2461397debfded082a916712e815f9c7f43b0d16
Tomaz Muraus <tomaz@tomaz.me>
2012-06-08 20:55:25 -0700

    Add a test case for it.

--- tests/test-traceroute.lua
+++ tests/test-traceroute.lua
@@ -204,19 +204,45 @@ exports['test_traceroute_error_invalid_hostname'] = function(test, asserts)
   end)
 end
 
-exports['test_traceroute_error_ipv6_not_supported'] = function(test, asserts)
+exports['test_traceroute_ipv6'] = function(test, asserts)
   local hopCount = 0
-  local tr = Traceroute:new('2001:0db8:85a3:0000:0000:8a2e:0370:7334', {})
-  Traceroute._spawn = exports.getEmitter('./tests/fixtures/error_ipv6_not_supported.txt', true)
+  local splitHops = {}
+  local hopNumber = 0
+
+  local tr = Traceroute:new('2607:f8b0:4009:803::1000', {})
+  Traceroute._spawn = exports.getEmitter('./tests/fixtures/output_ipv6_split_routes.txt', false)
   tr:traceroute()
 
   tr:on('hop', function(hop)
     hopCount = hopCount + 1
+    hopNumber = hop['number']
+
+    if not splitHops[hopNumber] then
+      splitHops[hopNumber] = 0
+    end
+
+    splitHops[hopNumber] = splitHops[hopNumber] + 1
+
+    if hopCount == 2 then
+        asserts.equals(hop['number'], 2)
+        asserts.equals(hop['ip'], '2001:4801:800:c3:601a:2::')
+        asserts.dequals(hop['rtts'], {0.974, 1.147, 1.150})
+    elseif hopNumber == 3 then
+      if splitHops[hopNumber] == 1 then
+        asserts.equals(hop['ip'], '2001:4801:800:cb:c3::')
+        asserts.dequals(hop['rtts'], {0.766})
+      elseif splitHops[hopNumber] == 2 then
+        asserts.equals(hop['ip'], '2001:4801:800:ca:c3::')
+        asserts.dequals(hop['rtts'], {0.773})
+      elseif splitHops[hopNumber] == 3 then
+        asserts.equals(hop['ip'], '2001:4801:800:cb:c3::')
+        asserts.dequals(hop['rtts'], {0.954})
+      end
+    end
   end)
 
-  tr:on('error', function(err)
-    asserts.equals(hopCount, 0)
-    asserts.ok(err.message:find('Address family for hostname not supported'))
+  tr:on('end', function(err)
+    asserts.equals(hopNumber, 10)
     test.done()
   end)
 end
